bee7a292c1bc8291aa9360e643f311141125a259
Duckphuong <phucloctho054@gmail.com>
2024-09-18 19:27:08 +0700

    bai63 xay dung header p1

--- src/component/Layout/defaultLayout/SideBar/index.js
+++ src/component/Layout/defaultLayout/SideBar/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Side Bar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
