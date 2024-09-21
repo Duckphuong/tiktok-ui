14aa4806a2a54c78656f93bac6137658f071fe3c
Duckphuong <phucloctho054@gmail.com>
2024-09-21 19:17:59 +0700

    bai 73 xay dung logic header phan 4

---
+++ src/apiServices/searchServices.js
@@ -0,0 +1,30 @@
+import * as request from '~/utils/request';
+
+export const search = async (q, type = 'less') => {
+    try {
+        const res = await request.get(`users/search`, {
+            params: {
+                q,
+                type,
+            },
+        });
+        return res.data;
+    } catch (error) {
+        console.log('error');
+    }
+};
+
+
+export const search = async (q, type = 'less') => {
+    try {
+        const res = await request.get(`users/search`, {
+            params: {
+                q,
+                type,
+            },
+        });
+        return res.data;
+    } catch (error) {
+        console.log('error');
+    }
+};
